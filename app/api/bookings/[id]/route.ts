b415dcad506bb7b76aef7d30e09a0e3c57dbae4d
Gen0miX <contact@jonas-pilloud.ch>
2025-08-26 16:03:34 +0200

    added await

--- app/api/bookings/[id]/route.ts
+++ app/api/bookings/[id]/route.ts
@@ -3,9 +3,9 @@ import { createClient } from "@/utils/supabase/server";
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: string } }
+   { params }: { params: Promise<{ id: string }> }
 ) {
-  const { id } = params;
+  const { id } = await params;
   const parsedId = parseInt(id);
   const updates = await request.json();
   const supabase = await createClient();
